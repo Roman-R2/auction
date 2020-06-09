6088e08207f3f9f107215c8dd736a77afee63b31
Roman <slyusarrv@yandex.ru>
2020-06-09 15:34:36 +0500

    First action in slim like slim requires

--- api/public/index.php
+++ api/public/index.php
@@ -2,19 +2,27 @@
 
 declare(strict_types=1);
 
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Http;
 use \Slim\Factory\AppFactory;
-//http_response_code(500);
+
+http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
 
-$app->get('/', function (Request $request, Response $response, $args){
-   $response->getBody()->write('{}');
-   return $response->withHeader('Content-type',' application\json');
-});
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
+
+$app->get('/', Http\Action\HomeAction::class);
 
 $app->run();
---
+++ api/src/Http/Action/HomeAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Action;
+
+use App\Http;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use stdClass;
+
+class HomeAction
+{
+    function __invoke (Request $request, Response $response, $args) : Response
+    {
+        return Http::json($response, new StdClass());
+    }
+}
