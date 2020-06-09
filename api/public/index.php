2756d941e4a27ffbe2714cca7fcc9b5f54d25a3c
Roman <slyusarrv@yandex.ru>
2020-06-09 14:05:53 +0500

    Install slim

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
-header('Content-type: application\json');
 
-echo "{}";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use \Slim\Factory\AppFactory;
+//http_response_code(500);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args){
+   $response->getBody()->write('{}');
+   return $response->withHeader('Content-type',' application\json');
+});
+
+$app->run();
