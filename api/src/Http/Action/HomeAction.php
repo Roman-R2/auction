c9f17fc87775fcc1aed444d9e31a91179039f492
Roman <slyusarrv@yandex.ru>
2020-06-10 10:24:30 +0500

    Add php-di autowiring

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -6,11 +6,8 @@ namespace App\Http\Action;
 
 use App\Http;
 use App\JsonResponse;
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use stdClass;
 
