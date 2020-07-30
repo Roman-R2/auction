095a1176f02236497f684d17bc6c2f5b8a561e60
Roman <slyusarrv@yandex.ru>
2020-07-30 11:49:41 +0500

    Added validation rules

--- api/config/common/validator.php
+++ api/config/common/validator.php
@@ -10,6 +10,8 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 // и возвращаем его в наш код
 return [
     ValidatorInterface::class => function (): ValidatorInterface {
+        //Вызываем этот метод, так как у нас doctrine 2, и она не может просто так
+        // подкгрузить аннотации из наших комманд, так что мы делаем это здесь
         /** @psalm-suppress DeprecatedMethod */
         AnnotationRegistry::registerLoader('class_exists');
 
