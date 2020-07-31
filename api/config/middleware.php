91a6deed4e0014254aa9e36efc866d8551a294fb
Roman <slyusarrv@yandex.ru>
2020-07-31 09:57:54 +0500

    Extracted locale negotiation

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -12,11 +12,12 @@ return static function (App $app): void {
     //Свой собственный посредник, который отлавливает ошибки валидации,
     // чтобы не копипастить код по валидации в наших actions
     $app->add(Middleware\ValidationExceptionHandler::class);
-    //Свой собственный посредник, который устанавливает язык переводчика
-    // исходя из заголовка Accept-Language у HTTP запроса
-    $app->add(Middleware\TranslatorLocale::class);
     //Свой собственный посредник для очистки строк от пробелов и от не загруженных файлов
     $app->add(Middleware\ClearEmptyInput::class);
+    //Свой собственный посредники, которые устанавливают язык переводчика
+    // исходя из заголовка Accept-Language у HTTP запроса
+    $app->add(Middleware\TranslatorLocale::class);
+    $app->add(Middleware\LocaleNegotiation::class);
     //Посредник, которые парсит наш body запроса из разных форматов (таких как xml, json...)
     //В зависимости от того, какой запрос прилетел, производится декодирование запроса
     $app->addBodyParsingMiddleware();
